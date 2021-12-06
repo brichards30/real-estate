17ea28b0495220c9139b84f56b184f11a562e2ea
Brittany Richards <brittany.m.richards3@gmail.com>
2021-12-06 15:52:04 -0600

    house images loaded, imported image from assets

--- components/Property.jsx
+++ components/Property.jsx
@@ -5,12 +5,13 @@ import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified} from 'react-icons/go';
 import millify from 'millify';
+import DefaultImage from '../assets/images/house.jpeg'
 
 const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalId } }) => (
     <Link href={`/property/${externalId}`} passHref>
        <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
            <Box>
-               <Image src={coverPhoto ? coverPhoto.url : defaultImage} />
+               <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt="house"/>
 
            </Box>
 

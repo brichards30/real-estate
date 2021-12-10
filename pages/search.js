dee021c7966f8538c60b35a5b0043e8bdd3b9bb0
Brittany Richards <brittany.m.richards3@gmail.com>
2021-12-10 17:19:19 -0600

    added no results image to search page

--- pages/search.js
+++ pages/search.js
@@ -1,13 +1,15 @@
 import {useState} from 'react';
-import {useRouter} from 'react';
+import {useRouter} from "next/router";
 import Image from 'next/image';
 import {Flex, Box, Text, Icon} from '@chakra-ui/react';
 import {BsFilter} from 'react-icons/bs';
 import SearchFilters from "../components/SearchFilters";
+import Property from "../components/Property";
+import noresult from '../assets/images/noresult.svg';
 
 const Search = () => {
     const [searchFilters, setSearchFilters] = useState(false);
-    const router = useRouter;
+    const router = useRouter();
 
     return (
         <Box>
@@ -26,7 +28,19 @@ const Search = () => {
                 <Text>Search Property by Filters</Text>
                 <Icon paddingLeft="2" w="7" as={BsFilter}/>
             </Flex>
-            {searchFilters && <SearchFilters/>}
+            {searchFilters && <SearchFilters />}
+            <Text fontSize="2xl" p="4" fontWeight="bold">
+                Properties {router.query.purpose}
+            </Text>
+            <Flex flexWrap="wrap">
+                {[].map((property) => <Property property={property} key={property.id}/>)}
+            </Flex>
+            {[].length === 0 && (
+                <Flex justifyContent="center" alignItems="center" flexDirection="column" marginTop="5" marginBottom="5">
+                    <Image alt="no result" src={noresult}/>
+                    <Text fontSize="2xl" marginTop="3">No results found</Text>
+                </Flex>
+            )}
         </Box>
     )
 };
